c74a322a24e618c0963eb2f3e3d5aa732c64c909
Max He <he_shilin@hotmail.com>
2012-11-06 20:15:26 -0700

    Pushing new changes

--- source/prep/utility/filtering/ComparerExtensions.cs
+++ source/prep/utility/filtering/ComparerExtensions.cs
@@ -5,9 +5,27 @@ namespace prep.utility.filtering
 {
   public static class ComparerExtensions
   {
-    public static IComparer<TItemToSort> then_by<TItemToSort, TPropertyTypeToSortOn>(this IComparer<TItemToSort> comparer,PropertyAccessor<TItemToSort,TPropertyTypeToSortOn> accessor)
+    public static IComparer<TItemToSort> then_by<TItemToSort, TPropertyTypeToSortOn>(this IComparer<TItemToSort> comparer,PropertyAccessor<TItemToSort,TPropertyTypeToSortOn> accessor) where TPropertyTypeToSortOn : IComparable<TPropertyTypeToSortOn>
     {
-      throw new NotImplementedException(); 
+      return new ThenComparer<TItemToSort, TPropertyTypeToSortOn>(comparer, accessor);
+    }
+  }
+
+  public class ThenComparer<TTypeToSort, TPropertyTypeToSortOn> : IComparer<TTypeToSort>
+    where TPropertyTypeToSortOn : IComparable<TPropertyTypeToSortOn>
+  {
+    PropertyAccessor<TTypeToSort, TPropertyTypeToSortOn> accessor;
+    private IComparer<TTypeToSort> _previous;
+
+    public ThenComparer(IComparer<TTypeToSort> previous, PropertyAccessor<TTypeToSort, TPropertyTypeToSortOn> accessor)
+    {
+      _previous = previous;
+      this.accessor = accessor;
+    }
+
+    public int Compare(TTypeToSort x, TTypeToSort y)
+    {
+      return _previous.Compare(x, y) == 0 ? accessor(x).CompareTo(accessor(y)) : _previous.Compare(x, y);
     }
   }
 }
